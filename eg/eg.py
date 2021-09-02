69f9ddb72bc995f7701b14e01565535cb0602160
Rodrigo Ramón <79613034+velazquezr35@users.noreply.github.com>
2021-09-01 22:55:16 -0300

    e Loads - t
    Ahora, la tabla de fuerzas se acomoda al array t_stru

--- eg/eg.py
+++ eg/eg.py
@@ -47,24 +47,22 @@ runing things
 case1=sim() # this will contain data corresponding to a particular simulation (a "study case") under analysis
 case1.fName='eg' # output binary file name (without extension)
 case1.stru.name='str ex raw data'
-case1.stru.nodes=[200001, 200002] # nodes of interest - only information relative to these nodes is going to be read
-case1.stru.struRdOpt = 'bin' # set flag for reading ASCII file - default 'bin'
+case1.stru.nodes=[200000, 200001] # nodes of interest - only information relative to these nodes is going to be read
+case1.stru.struRdOpt = 'raw' # set flag for reading ASCII file - default 'bin'
 case1.stru.p11FN='pcolgante.@1' # binary *.p11 file name (without extension - Simpact output) from wich extract generalized displacements
 case1.stru.rsnDe='pcolgante' # ASCII *.rsn file name (without extension) - Delta output
 case1.stru.loadsFN = 'AeroFcsOnStruc' #ASCII *.dat file name - Loads
-case1.stru.loadRdOpt = 'bin'
+case1.stru.loadRdOpt = 'raw'
 case1.stru.struEigOpt = True
 case1.stru.loadEigOpt = True
-case1.stru.intLabOffset = 24
+case1.stru.intLabOffset = 6
 #data dir: use / instead of \
 # case1 = epp(case1, **{'subDir_P11':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg_advance/020 aeroelástica vigas rígido/frecuencias y modos/', 'subDir_RSN':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg_advance/020 aeroelástica vigas rígido/frecuencias y modos/', 'subDir_FCS':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg_advance/020 aeroelástica vigas rígido/frecuencias y modos/', 'glob_print_output': True, 'BN_mode':'preserve'})
 case1 = epp(case1, **{'subDir_P11':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'subDir_RSN':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'subDir_FCS':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'BN_mode':'preserve'})
 
-case1.stru = upd_phiR(case1.stru, **{'MOI':[1],'subDir_P11':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'subDir_RSN':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'subDir_FCS':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'BN_mode':'preserve'})
+#update manual
+# case1.stru = upd_phiR(case1.stru, **{'MOI':[1,5],'subDir_P11':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'subDir_RSN':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'subDir_FCS':'C:/Users/Ramon/Documents/ayudantia/code/28-08-21/eg/subF/', 'BN_mode':'preserve'})
 
-plotter.fig_qt(case1.stru, [[1,2]])
-# case1.stru = time_slice(case1.stru)
-# case1.stru = modal_w(case1.stru)
 
 ## Plot
 #plot opts
